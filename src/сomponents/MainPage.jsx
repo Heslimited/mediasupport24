b1088dd3c320bc0724964d998b199a77a5db7c89
Heslimited <dimzo333@gmail.com>
2024-03-31 04:27:23 +0300

    Fixes main page

--- src/сomponents/MainPage.jsx
+++ src/сomponents/MainPage.jsx
@@ -4,11 +4,20 @@ import diagram from '../assets/diagram.svg'
 import cloud from '../assets/cloud.svg'
 import fannel from '../assets/fannel.svg'
 import SMM from '../assets/SMM.svg'
+
+import AlexFitness from '../assets/Alex-Fitness.svg'
+import LogoACB from '../assets/logo-acb.svg'
+import LogoAKB from '../assets/logoakb.svg'
+import LogoXS from '../assets/logo-xs.svg'
+
+import VectorLeft from '../assets/VectorLeft.svg'
+import VectorRight from '../assets/VectorRight.svg'
+
 import '../css/blocks/Main.scss'
 
 export default function MainPage () {
     return (
-        <div>
+        <div className="mainpage">
             <div className="firstcon">
                 <h1>MEDIA SUPPORT 24</h1>
                 <h2>Мы предлагаем комплексные и индивидуальные <br>
@@ -84,7 +93,7 @@ export default function MainPage () {
                 </div>
             </div>
             <h2 className="title">КЕЙСЫ</h2>
-            <div className="fourthcon">
+            <div className="fourthcon-1">
                 <div className="case-1">
                     <h3>Видеосъемка</h3>
                 </div>
@@ -94,6 +103,8 @@ export default function MainPage () {
                 <div className="case-3">
                     <h3>Рекламные ролики</h3>
                 </div>
+            </div>
+            <div className="fourthcon-2">
                 <div className="case-4">
                     <h3>Таргетинг</h3>
                 </div>
@@ -101,9 +112,53 @@ export default function MainPage () {
                     <h3>SMM</h3>
                 </div>
                 <div className="case-6">
-                    <h3>Сайт</h3>
+                    <h3>Сайт</h3>                    
                 </div>
             </div>
+            <h2 className="title">НАШ БЛОГ</h2>
+            <div className="fifthcon">
+                <div className="blog-1">
+                    <div className="fifthcon-1">
+                        <h3>ХУДОЖНИКИ, <br></br>КОТОРЫЕ ЗАДАЮТ <br></br>ТОН ИНДУСТРИИ</h3>
+                    </div>
+                    <div>
+                        <p>Лучшие современные графические <br></br>дизайнеры</p>
+                    </div>
+                </div>
+                <div className="blog-2">
+                    <div className="fifthcon-2">
+                        <h3>ХУДОЖЕСТВЕННАЯ <br></br>РЕТУШЬ</h3>
+                    </div>
+                    <div>
+                        <p>Принципы хорошей рекламы <br></br>не меняются – дизайнерам лишь </p>
+                    </div>
+                </div>
+                <div className="blog-3">
+                    <div className="fifthcon-3">
+                        <h3>КРЕАТИВНАЯ КОМПЬЮТЕРНАЯ ГРАФИКА В РЕКЛАМЕ</h3>
+                    </div>
+                    <div> 
+                        <p>Верите в чудеса?<br></br>Думаете, что Ваше фото с ужасным  </p>
+                    </div>
+                </div>
+            </div>
+            <h2 className="title">НАШИ ПАРТНЕРЫ</h2>
+            <div className="sixthcon">
+                <div className="partners">
+                    <img src={AlexFitness} alt="Alex Fitness logo" />
+                    <img src={LogoACB} alt="ACB logo" />
+                    <img src={LogoAKB} alt="AKB logo" />
+                    <img src={LogoXS} alt="XS logo" />
+                </div>
+                <div className="review">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Venenatis dolor risus purus, id massa aenean. Amet eget ornare ut urna accumsan tellus. Lorem commodo enim facilisi a. Pellentesque pharetra pretium vestibulum, non scelerisque tempus amet sollicitudin.</p>
+                    <div>
+                        <img src></img>
+                        <h2>Руководитель сервиса Шишкин Лес</h2>
+                    </div>
+                </div>
+            </div>
+            <h2 className="title">НАШИ ПАРТНЕРЫ</h2>
         </div>
     )
 }
